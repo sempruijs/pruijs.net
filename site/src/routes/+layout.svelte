80fe2995dd6d1e2a0007f3ac329a23a83f4392f2
Sem Pruijs <sem@pruijs.net>
2026-09-23 13:12:08 +0200

    Add picture

--- site/src/routes/+layout.svelte
+++ site/src/routes/+layout.svelte
@@ -3,6 +3,6 @@
 	let { children } = $props();
 </script>
 
-<div class="min-h-screen bg-[#0a2540] text-white">
+<div class="min-h-screen bg-offwhite text-offblack dark:bg-offblack dark:text-offwhite transition-colors">
 {@render children()}
 </div>
